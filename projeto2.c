9d68340ead4fff390c8d73bd02cc8f652545d481
StiveRibeiro <111088782+StiveRibeiro@users.noreply.github.com>
2022-08-12 11:33:58 -0400

    Add files via upload
    Aqui eu só estava testando as funções for, while, etc.

---
+++ projeto2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//dia 12/08, continuando as aulas do w3schools, comecando em while, for, continue, etc
+
+int main() {
+ /*   int i = 0;
+
+    while (i <= 69420) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
+*/
+
+//agora indo pra arrays
+
+    int numerosDaora[] = {42, 69, 420, 69420};
+    int n;
+
+    for (n = 0; n < 4; n++) {
+    printf("%d\n", numerosDaora[n]);
+    }
+
+}
