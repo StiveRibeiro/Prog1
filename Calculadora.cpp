97d2a596c0285ac2a98c7f4c59814f9736e9bc5e
StiveRibeiro <111088782+StiveRibeiro@users.noreply.github.com>
2023-02-19 12:24:27 -0400

    Update Calculadora.cpp

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -5,143 +5,152 @@
 
 using namespace std;
 
-int opcao;
+int op;
 float n1, n2, n3;
-bool continuar = true;
-
-void conta(){
-
-    switch(opcao) {
-
-        case 1:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número: ";
-            cin>>n1;
-            cout<<"Segundo número: ";
-            cin>>n2;
-            n3 = n1 + n2;
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        case 2:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número: ";
-            cin>>n1;
-            cout<<"Segundo número: ";
-            cin>>n2;
-            n3 = n1 - n2;
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        case 3:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número: ";
-            cin>>n1;
-            cout<<"Segundo número: ";
-            cin>>n2;
-            n3 = n1 * n2;
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        case 4:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número: ";
-            cin>>n1;
-            cout<<"Segundo número: ";
-            cin>>n2;
-            n3 = n1 / n2;
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        case 5:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número(base): ";
-            cin>>n1;
-            cout<<"Segundo número(potência): ";
-            cin>>n2;
-            n3 = pow(n1,n2);
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        case 6:
-            cout<<"\nDigite os números a serem operados. \n";
-            cout<<"Primeiro número(base): ";
-            cin>>n1;
-            cout<<"Segundo número(raiz): ";
-            cin>>n2;
-            n2 = 1/n2;
-            n3 = pow(n1, n2);
-            cout<<"\nO resultado é: "<<n3<<"\n";
-            break;
-
-        default:
-            cout<<"Operação inválida.";
-            break;
-
-    }
-}
-
+char r;
 
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-    while(continuar){
     cout<<"|-----------MENU-----------|\n";
     cout<<"|   1 - SOMA               |\n";
-    cout<<"|   2 - SUBTRAÇÃO          |\n";
-    cout<<"|   3 - MULTIPLICAÇÃO      |\n";
-    cout<<"|   4 - DIVISÃO            |\n";
-    cout<<"|   5 - POTENCIAÇÃO        |\n";
-    cout<<"|   6 - RADICIAÇÃO         |\n";
+    cout<<"|   2 - SUBTRAÃ‡ÃƒO          |\n";
+    cout<<"|   3 - MULTIPLICAÃ‡ÃƒO      |\n";
+    cout<<"|   4 - DIVISÃƒO            |\n";
+    cout<<"|   5 - POTENCIAÃ‡ÃƒO        |\n";
+    cout<<"|   6 - RADICIAÃ‡ÃƒO         |\n";
     cout<<"|   7 - SAIR               |\n";
     cout<<"|-----------====-----------|\n";
-    cout<<"Escolha uma opção: ";
-    cin>>opcao;
+    cout<<"Escolha uma opÃ§Ã£o: ";
+    cin>>op;
 
-    switch(opcao){
+    switch(op){
 
         case 1:
-            cout<<"\nOpção escolhida SOMA\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida SOMA\n";
             break;
 
         case 2:
-            cout<<"\nOpção escolhida SUBTRAÇÃO\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida SUBTRAÃ‡ÃƒO\n";
             break;
 
         case 3:
-            cout<<"\nOpção escolhida MULTIPLICAÇÃO\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida MULTIPLICAÃ‡ÃƒO\n";
             break;
 
         case 4:
-            cout<<"\nOpção escolhida DIVISÃO\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida DIVISÃƒO\n";
             break;
 
         case 5:
-            cout<<"\nOpção escolhida POTENCIAÇÃO\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida POTENCIAÃ‡ÃƒO\n";
             break;
 
         case 6:
-            cout<<"\nOpção escolhida RADICIAÇÃO\n";
-            conta();
+            cout<<"\nOpÃ§Ã£o escolhida RADICIAÃ‡ÃƒO\n";
             break;
 
 
         case 7:
-            cout<<"\nOpção escolhida SAIR\n";
-            continuar = false;
+            cout<<"\nOpÃ§Ã£o escolhida SAIR\n";
+            return 0;
             break;
 
         default:
-            cout<<"\nOpção inválida\n";
+            cout<<"\nOpÃ§Ã£o invÃ¡lida\n";
             main();
     }
-    }
 
-return 0;
+        switch(op) {
+
+        case 1:
+            cout<<"\nDigite os nÃºmeros a serem operados. \n";
+            cout<<"Primeiro nÃºmero: ";
+            cin>>n1;
+            cout<<"Segundo nÃºmero: ";
+            cin>>n2;
+            n3 = n1 + n2;
+            cout<<"\nO resultado Ã©: "<<n3<<"\n";
+            break;
+
+        case 2:
+            cout<<"\nDigite os nÃºmeros a serem operados. \n";
+            cout<<"Primeiro nÃºmero: ";
+            cin>>n1;
+            cout<<"Segundo nÃºmero: ";
+            cin>>n2;
+            n3 = n1 - n2;
+            cout<<"\nO resultado Ã©: "<<n3<<"\n";
+            break;
+
+        case 3:
+            cout<<"\nDigite os nÃºmeros a serem operados. \n";
+            cout<<"Primeiro nÃºmero: ";
+            cin>>n1;
+            cout<<"Segundo nÃºmero: ";
+            cin>>n2;
+            n3 = n1 * n2;
+            cout<<"\nO resultado Ã©: "<<n3<<"\n";
+            break;
+
+        case 4:
+            cout<<"\nDigite os nÃºmeros a serem operados. \n";
+            cout<<"Primeiro nÃºmero: ";
+            cin>>n1;
+            cout<<"Segundo nÃºmero: ";
+            cin>>n2;
+            n3 = n1 / n2;
+            cout<<"\nO resultado Ã©: "<<n3<<"\n";
+            break;
+
+        case 5:
+            cout<<"\nDigite os nÃºmeros a serem operados. \n";
+            cout<<"Primeiro nÃºmero(base): ";
+            cin>>n1;
+            cout<<"Segundo nÃºmero(potÃªncia): ";
+            cin>>n2;
+            n3 = pow(n1,n2);
+            cout<<"\nO resultado Ã©: "<<n3<<"\n";
+            break;
+
+        case 6:
+            cout<<"|  1 - Raiz quadrada  |\n";
+            cout<<"|  2 - Raiz cÃºbica    |\n";
+            cout<<"|  3 - Outra raiz     |\n";
+            cin>>op;
+
+            switch(op) {
+
+                case 1:
+                    cout<<"Digite o nÃºmero a ser operado. \n";
+                    cin>>n1;
+                    n2 = sqrt(n1);
+                    cout<<"O resultado Ã©: "<<n2<<"\n";
+                    break;
+
+                case 2:
+                    cout<<"Digite o nÃºmero a ser operado. \n";
+                    cin>>n1;
+                    n2 = cbrt(n1);
+                    cout<<"O resultado Ã©: "<<n2<<"\n";
+                    break;
+
+                case 3:
+                    cout<<"\nDigite os nÃºmeros a serem operados. \n";
+                    cout<<"Primeiro nÃºmero(base): ";
+                    cin>>n1;
+                    cout<<"Segundo nÃºmero(raiz): ";
+                    cin>>n2;
+                    n3 = pow(n1, 1/n2);
+                    cout<<"\nO resultado Ã©: "<<n3<<"\n";
+                    break;
+
+                default:
+                    cout<<"\nOpÃ§Ã£o invÃ¡lida.\n";
+                    main();
+            }
+            break;
+    }
+    main();
 }
